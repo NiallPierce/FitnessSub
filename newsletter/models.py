a2f16b1f5c1d2b61a8501756778937fc323c1625
Niall Pierce <niallpierce92@hotmail.com>
2025-04-15 08:31:35 +0000

    Add newsletter subscription functionality: - Created NewsletterSubscription model - Added newsletter signup form - Implemented signup view with success/error messages - Created newsletter signup template - Added URL routing for newsletter features

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(max_length=254, unique=True)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterForm
+from .models import NewsletterSubscription
 
-# Create your views here.
+def newsletter_signup(request):
+    """ Handle newsletter subscription """
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                messages.success(request, 'Thank you for subscribing to our newsletter!')
+                return redirect('home')
+            except:
+                messages.error(request, 'This email is already subscribed to our newsletter.')
+        else:
+            messages.error(request, 'Please enter a valid email address.')
+    else:
+        form = NewsletterForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/signup.html', context)
