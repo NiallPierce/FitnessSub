14b60c8918246b8b558590e9eb0d2abfa96fb945
Niall Pierce <niallpierce92@hotmail.com>
2025-04-20 10:48:39 +0000

    feat: Add featured products section to home page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    featured_products = Product.objects.filter(is_featured=True, is_available=True)[:6]
+    context = {
+        'featured_products': featured_products,
+    }
+    return render(request, 'home/index.html', context)
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
