9558a4687955cedc5305ac75367b10bd49cc8e89
Niall Pierce <niallpierce92@hotmail.com>
2025-04-21 10:56:46 +0000

    Add missing category_detail URL pattern

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('category/<slug:category_slug>/', views.category_detail, name='category_detail'),
     
     # Subscription URLs
     path('subscription-plans/', views.subscription_plans, name='subscription_plans'),
